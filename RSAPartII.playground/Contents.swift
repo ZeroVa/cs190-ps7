e67aadefed8d5268b016097682cc7b036ec8a9e5
Ahsan Zahid <pie.is.forever@gmail.com>
2016-05-04 23:59:55 -0700

    finished

--- RSAPartII.playground/Contents.swift
+++ RSAPartII.playground/Contents.swift
@@ -190,12 +190,32 @@ class RSA: Crypto {
  ## Implementation of RSA.privateKey() and RSA.decrypt() */
     // The private key is used for decryption.
     func privateKey() -> PrivateKey {
-        return PrivateKey(decryptionExponent: 2, modulus: 3)
+        let modulus = p * q
+        let f_n = (p - 1) * (q - 1)
+        let coprimeOptions = coprimes(f_n)
+        let encryptionExponent = coprimeOptions[1]
+        
+        let decryptionExponent = (1/encryptionExponent)%f_n
+        var currentNum = 0;
+        while(true){
+            if (currentNum*encryptionExponent)%f_n==1 {
+                break
+            }
+            else{
+                currentNum = currentNum + 1
+            }
+        }
+        return PrivateKey(decryptionExponent: currentNum, modulus: modulus)
     }
     
     // Decrypts cipherValue using the private key. Returns the plain value.
     func decrypt(cipherValue: Int) -> Int {
-        return 0
+        let privateKey = self.privateKey()
+        var exponentiated = 1
+        for _ in 0 ..< privateKey.decryptionExponent {
+            exponentiated *= cipherValue
+        }
+        return exponentiated % (p*q)
     }
     
 }
